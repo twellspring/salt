944b3114abd365ab2271482dbf139973372b8ad4
Todd Wells <twells@riskalyze.com>
2017-09-17 17:18:27 -0700

    elbv2 state create/delete alb WIP

--- salt/states/boto_elbv2.py
+++ salt/states/boto_elbv2.py
@@ -2,9 +2,7 @@
 '''
 Manage AWS Application Load Balancer
 
-.. versionadded:: 2017.7.0
-
-Add and remove targets from an ALB target group.
+Create and destroy Application Load Balancers ( ALBs) 
 
 This module uses ``boto3``, which can be installed via package, or pip.
 
@@ -34,6 +32,60 @@ passed in as a dict, or as a string to pull from pillars or minion config:
         region: us-east-1
 '''
 
+Created all three parts of the Applicaiton Load Balancer: Load Balancer, listeners, rules. 
+
+'''
+.. code-block:: yaml
+
+    Ensure myalb ALB exists:
+        boto_elbv2.present:
+            - name: myExternalALB
+            - vpc: myVPC
+            - region: us-east-1
+            - availability_zones:
+                - us-east-1a
+                - us-east-1c
+                - us-east-1d
+            - keyid: GKTADJGHEIQSXMKKRBJ08H
+            - key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
+            - listeners:
+                - port: 443
+                  protocol: HTTPS
+                  certificate: mySslCert
+                  ssl_policy:  SSL_TLS_GET_REAL_NAME_OF_THIS
+                  default_tg: www 
+                  rules:
+                    = condition_type:  host-header
+                      condition_value: www.mycompany.com
+                      priority: 2
+                      target_group: www
+                    - condition_type: path-pattern
+                      condition_value: /blog
+                      priority; 1
+                      target_group: blog  
+            - target_groups:
+                - name: www
+                  protocol: https
+                  port: 443
+                  health_check_protocol='HTTPS',
+                  health_check_path='/status',
+                  targets:
+                    - name: www1-prod
+                    - name: www2-prod
+               - name: blog
+                  protocol: http
+                  port: 80
+                  health_check_protocol='HTTP',
+                  health_check_path='/status',
+                  targets:
+                    - name: blog1-prod
+                      port: 8080
+                    - name: blog2-prod
+                      port: 8080
+                                      
+'''
+
+
 from __future__ import absolute_import
 
 # Import Python Libs
@@ -55,6 +107,126 @@ def __virtual__():
     return (False, "The boto_elbv2 module cannot be loaded: boto3 library not found")
 
 
+def create(name, subnets, security_groups=None, scheme='internet-facing',
+           tags=None, ip_type='ipv4', region=None, key=None, keyid=None,
+           profile=None, **kwargs):
+
+    '''
+    Create application load balancer if not present.
+
+    .. versionadded:: 2017.11.0
+
+    name
+        (string) - The name of the applicaiton load balancer ( ALB ).
+    subnets
+        (list) - The names of the subnets to attach to the ALB. You can
+                 specify only one subnet per Availability Zone. You must specify
+                 subnets from at least two Availability Zones.
+    security_groups
+        (list) - The names or IDs of the security groups to assign to the ALB.
+
+    scheme
+        (string) - Is the ALB ``internet-facing`` ( use public IP ) or
+                   ``internal`` ( use private IP )
+    tags
+        (dict) - tags to assing to the ALB.
+
+    ip_type
+        (string) -  The type of IP addresses used by the subnets associated with
+                    the application load balancer. The possible values are ``ipv4``
+                    (for IPv4 addresses) and ``dualstack`` (for IPv4 and IPv6 addresses).
+                    Internal load balancers must use ipv4.
+
+    CLI example:
+    .. code-block:: yaml
+
+        create-elb:
+          boto_elb2.create:
+            - name: myExternalALB
+            - subnets:
+              - us-east-1
+              - us-east-2
+            - security_groups:
+              - mysecgroup1
+            - ip_type: dualstack
+    '''
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+
+    if __salt__['boto_elbv2.exists'](name, region, key, keyid, profile):
+        ret['result'] = True
+        ret['comment'] = 'Application Load Balancer {0} already exists'.format(name)
+        return ret
+
+    if __opts__['test']:
+        ret['comment'] = 'Application Load Balancer {0} will be created'.format(name)
+        return ret
+
+    state = __salt__['boto_elbv2.create'](name,
+                                          subnets,
+                                          security_groups=security_groups,
+                                          scheme=scheme,
+                                          tags=tags,
+                                          ip_type=ip_type,
+                                          region=region,
+                                          key=key,
+                                          keyid=keyid,
+                                          profile=profile,
+                                          **kwargs)
+
+    if state:
+        ret['changes']['application_load_balancer'] = name
+        ret['result'] = True
+        ret['comment'] = 'Application Load Balancer {0} created'.format(name)
+    else:
+        ret['result'] = False
+        ret['comment'] = 'Application Load Balancer {0} creation failed'.format(name)
+    return ret
+
+
+
+def delete(name, region=None, key=None, keyid=None, profile=None):
+    '''
+    Delete Application Load Balancer.
+
+    name
+        (string) - The Name or Amazon Resource Name (ARN) of the Applicaiton Load Balancer.
+
+    CLI example:
+
+    .. code-block:: bash
+
+        delete-alb:
+          boto_elb2.delete:
+            - name: myExternalALB
+    '''
+
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+
+    if not __salt__['boto_elbv2.exists'](name, region, key, keyid, profile):
+        ret['result'] = True
+        ret['comment'] = 'Application Load Balancer {0} does not exists'.format(name)
+        return ret
+
+    if __opts__['test']:
+        ret['comment'] = 'Application Load Balancer {0} will be deleted'.format(name)
+        return ret
+
+    state = __salt__['boto_elbv2.delete'](name,
+                                          region=region,
+                                          key=key,
+                                          keyid=keyid,
+                                          profile=profile)
+
+    if state:
+        ret['result'] = True
+        ret['changes']['application_load_balancer'] = name
+        ret['comment'] = 'Application Load Balancer {0} deleted'.format(name)
+    else:
+        ret['result'] = False
+        ret['comment'] = 'Application Load Balancer {0} deletion failed'.format(name)
+    return ret
+
+
 def create_target_group(name, protocol, port, vpc_id,
                         region=None, key=None, keyid=None, profile=None,
                         health_check_protocol='HTTP', health_check_port='traffic-port',
@@ -63,9 +235,9 @@ def create_target_group(name, protocol, port, vpc_id,
                         unhealthy_threshold_count=2, **kwargs):
 
     '''
-    .. versionadded:: 2017.11.0
-
     Create target group if not present.
+
+    .. versionadded:: 2017.11.0
 
     name
         (string) - The name of the target group.
@@ -282,6 +454,8 @@ def targets_registered(name, targets, region=None, key=None, keyid=None,
 def targets_deregistered(name, targets, region=None, key=None, keyid=None,
                          profile=None, **kwargs):
     '''
+    .. versionadded:: 2017.7.0
+
     Remove targets to an Application Load Balancer target group.
 
     name
@@ -289,8 +463,6 @@ def targets_deregistered(name, targets, region=None, key=None, keyid=None,
 
     targets
         A list of target IDs or a string of a single target registered to the target group to be removed
-
-    .. versionadded:: Unknown
 
     .. code-block:: yaml
 
